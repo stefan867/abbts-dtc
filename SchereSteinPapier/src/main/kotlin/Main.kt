5c794573fe7c7a2ac4344fe885a39d29b7966a32
Andreas Wassmer <andreas.wassmer@fhnw.ch>
2024-11-20 09:01:43 +0100

    Added project SchereSteinPapier

---
+++ SchereSteinPapier/src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val options = listOf("s", "t", "p")
+    println("Willkommen zu Schere-Stein-Papier")
+    print("Wie heissen Sie? ")
+    val username = scanner.nextLine()
+
+    while (true) {
+        println("$username, wählen Sie: (S)chere, S(T)ein oder (P)apier (oder tippe '(B)eenden', um das Spiel zu verlassen")
+        val userChoice = scanner.nextLine().lowercase()
+
+        if (userChoice == "b") {
+            println("Das Spiel ist beendet.")
+            break
+        }
+
+        if (userChoice !in options) {
+            println("Ungültige Eingabe. Bitte wählen Sie nochmals.")
+            continue
+        }
+
+        val computerChoice = options.random()
+        when {
+            computerChoice == "s" -> println("Computer hat Schere gewählt")
+            computerChoice == "t" -> println("Computer hat Stein gewählt")
+            computerChoice == "p" -> println("Computer hat Papier gewählt")
+        }
+
+        when {
+            userChoice == computerChoice -> println("Unentschieden")
+            userChoice == "s" && computerChoice == "p" ||
+                    userChoice == "t" && computerChoice == "s" ||
+                    userChoice == "p" && computerChoice == "t" -> println("Sie gewinnen!")
+            else -> println("Der Computer gewinnt")
+        }
+        println()
+    }
+}
